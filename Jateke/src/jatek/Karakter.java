f0f01b90ae25186b3b1a4810c811aa61ea296d68
dkornel01 <kornel.dobrocsi@gmail.com>
2023-12-11 09:18:51 +0100

    UUID

--- Jateke/src/jatek/Karakter.java
+++ Jateke/src/jatek/Karakter.java
@@ -5,12 +5,22 @@
 package jatek;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  *
  * @author dobrocsi.kornel
  */
 public class Karakter {
+    Random rnd=new Random();
     private int eltero,ugyesseg,szerencse;
     private ArrayList<String> felszereles;
+
+    public Karakter(int eltero, int ugyesseg, int szerencse, ArrayList<String> felszereles) {
+        this.eltero =(rnd.nextInt(1,7))*2+12 ;
+        this.ugyesseg = (rnd.nextInt(1,7))+6;
+        this.szerencse = (rnd.nextInt(1,7))+6;
+        this.felszereles = felszereles;
+    }
+    
 }
